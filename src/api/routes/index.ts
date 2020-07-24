d0f55dc09a310663f9d65f9185a472545258a42a
RexterR <sumandas.workplace@gmail.com>
2020-07-24 12:24:40 +0530

    fixing

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,7 +1,10 @@
-import { Router } from "express";
+import { Router,Request,Response } from "express";
 import productRoute from "./product";
 import customerRoute from './customer'
 const router = Router();
+router.get("/",(req:Request,res:Response)=>{
+    res.send(`<h1>Hello Welcome here`)
+})
 router.use("/product",productRoute);
 router.use("/customer",customerRoute)
 export { router as default };
